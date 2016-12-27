5ebefa76c597c2410d5b33b1f5ac8bd2262eb1e3
Marcus <marcus0308@live.hk>
2016-12-27 18:49:15 +0000

    1

--- Packages_Installer.R
+++ Packages_Installer.R
@@ -1,6 +1,6 @@
 list.of.packages <- c("XML", "rvest", "Hmisc", "MASS","RevoUtilsMath", "RevoUtils","sparkTable",
-                     "rio", "psych","readr","data.table","magrittr","sparklyr",
-                      "shiny","flexdashboard","gmodels","janitor", 
+                     "rio", "psych","readr","data.table","magrittr","sparklyr", "Rfacebook","RColorBrewer","RCurl","httpuv","httr","rjson",
+                      "shiny","flexdashboard","gmodels","janitor", "caret",
                       "foreach","profvis","sqldf","reshape2","dplyr","zoo",
                       "scales","RODBC", "readxl","openxlsx","doParallel","dplyr","tidyr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
