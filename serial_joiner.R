4b7a167345760d28efa67a9fc5b89f05ac49e094
Marcus <marcus0308@live.hk>
2016-12-30 00:09:12 +0000

    sfd

--- serial_joiner.R
+++ serial_joiner.R
@@ -7,4 +7,5 @@ serial.joiner <- function(fun = inner_join, list_of_df) {
   new_fun <- fun
     huge_table <- Reduce(function(...) new_fun(...),
                          list_of_df)
+    return(huge_table)
 }
