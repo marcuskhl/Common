aecf9237bc875482830e32dd0679d29b24ab7e19
Leung <Marcus.Leung@ihs.com>
2017-01-04 09:29:10 +0000

    added f2n fn

--- Functions_Installer.R
+++ Functions_Installer.R
@@ -1,11 +1,11 @@
 #Functions Installer
 as.df <- function(dt){
-  as.data.frame(dt)  
+  as.data.frame(dt)
 }
 
 as.dt <- function(dt){
-  as.data.table(dt)  
-}  
+  as.data.table(dt)
+}
 
 #This function removes columns from a table
 # pass in the df and the column and it will return a column without the mentioned column(s)
@@ -15,3 +15,9 @@ column.rm <- function(df,col_name){
 }
 
 print("Functions Loaded")
+
+
+# This function converts factor column to numeric (usually applicable to year)
+f2n <- function(col){
+	return(as.numeric(as.character(col)))
+}
