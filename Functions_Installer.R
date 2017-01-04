b0653b2287be21db8387a26a443f116d3e96c03a
Leung <Marcus.Leung@ihs.com>
2017-01-04 09:50:28 +0000

    added save.xlsx

--- Functions_Installer.R
+++ Functions_Installer.R
@@ -21,3 +21,20 @@ print("Functions Loaded")
 f2n <- function(col){
 	return(as.numeric(as.character(col)))
 }
+
+# This function saves multiple dfs into one workbook
+save.xlsx <- function (file, ...)
+{
+  wb <- createWorkbook()
+  require(openxlsx, quietly = TRUE)
+  objects <- list(...)
+  fargs <- as.list(match.call(expand.dots = TRUE))
+  objnames <- as.character(fargs)[-c(1, 2)]
+  nobjects <- length(objects)
+  for (i in 1:nobjects) {
+    addWorksheet(wb, sheetName = objnames[i])
+    writeData(wb, objnames[i], objects[[i]])
+  }
+  saveWorkbook(wb, file, overwrite = T)
+  print(paste("Workbook", file, "has", nobjects, "worksheets."))
+}
